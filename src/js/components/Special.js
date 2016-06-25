fd07a4e68504e470e6562bfa094f588c112f4a4b
allenwhale <allenwhale@yahoo.com.tw>
2016-06-25 21:24:52 +0800

    add special thanks

---
+++ src/js/components/Special.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Special extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                KCW<br/>
+                TRUCK<br/>
+                EASY<br/>
+            </div>
+        );
+    }
+}
